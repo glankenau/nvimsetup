4519e1e21113b8ab038ece9648ea2054c5ea2b86
Gerardo Lankenau <glankenau@hey.com>
2026-01-10 13:21:23 +0100

    Persist personal mac settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,10 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
+vim.opt.guicursor = ""
+vim.opt.nu = true
+vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.colorcolumn = "80"
 vim.opt.scrolloff = 8
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
